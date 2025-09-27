46bc33f475a0ca1c8803cb8898ffa28c94a8adbb
Alejandro <71718967+abeguerr01@users.noreply.github.com>
2025-09-27 17:53:01 +0200

    Add files via upload

---
+++ p1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1;
+
+/**
+ *
+ * @author USER
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        Calculadora c = new Calculadora();
+        
+        c.setVisible(true);
+        // v.setVisible(true);
+        System.out.println("Aplicacion funcionando correctamente");
+    }
+}
